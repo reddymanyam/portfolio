ca4a3db28af8f0bcece00b2d63f62d2b36c5c74c
reddymanyam <reddappareddy18@gmail.com>
2023-11-26 18:36:37 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+let btns = document.querySelectorAll(".category");
+let submit = document.getElementById("submit");
+
+function downlodeBtn(){
+    alert("CV Downloded")
+}
+
+submit.addEventListener("click", function(){
+    let email = document.getElementById("email");
+    let name = document.getElementById("name");
+    let msg = document.getElementById("msg");
+
+    if(email.value == "" || name.value == "" || msg.value == "" ){
+        alert("Please Fill Details")
+    }else{
+        alert("Succesfully Connected")
+    }
+
+})
